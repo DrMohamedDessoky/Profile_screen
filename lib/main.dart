ba6433cb1e8168eca9d08b61e5e08c547a70a493
DrMohamedDessoky <mohamed.dessoky@outlook.com>
2025-02-09 00:58:21 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/my_app.dart';
+
+void main() {
+  runApp(MyApp(
+  
+  ));
+}
---
+++ lib/my_screen.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+class MyScreen extends StatelessWidget {
+  const MyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+        title: Center(
+            child: Text(
+          "Profile Screen",
+          style: TextStyle(color: Colors.white),
+        )),
+        actions: [
+          Icon(
+            Icons.edit,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            CircleAvatar(
+              radius: 80,
+              backgroundImage: AssetImage("assets/images/ropot.jpg"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Mohamed Dessoky",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Text(
+              "mohamed.dessoky@gmail.com",
+              style: TextStyle(color: Colors.grey.shade400, fontSize: 15),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Divider(
+              color: Colors.white,
+              thickness: 2,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  color: Colors.grey, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Settings",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  color: Colors.grey, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Friends",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  color: Colors.grey, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.group,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "New Group",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                  color: Colors.grey, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.support_agent,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Support",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/facebook.png",
+                  height: 30,
+                  width: 30,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Image.asset(
+                  "assets/images/twitter.png",
+                  height: 30,
+                  width: 30,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Image.asset(
+                  "assets/images/tiktok.png",
+                  height: 30,
+                  width: 30,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Image.asset(
+                  "assets/images/instagram.png",
+                  height: 30,
+                  width: 30,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
